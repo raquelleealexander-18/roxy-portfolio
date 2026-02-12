155899563d94ca7f5ccaa8ba75ab5f6f473ff34a
raquelleealexander-18 <raquelleealexander@gmail.com>
2026-02-12 17:59:56 -0500

    Create posts.js

---
+++ posts.js
@@ -0,0 +1,15 @@
+const blogPosts = [
+    {
+        title: "The Ethics of Digital Accessibility",
+        date: "February 12, 2026",
+        excerpt: "A reflection on how smart-city technologies often overlook the navigational needs of the unhoused...",
+        link: "blog/post1.html"
+    },
+    {
+        title: "Reflections on Philosophy & Care",
+        date: "January 20, 2026",
+        excerpt: "Exploring how AI-driven assistive tools change the interpersonal dynamics of caregiving.",
+        link: "blog/post2.html"
+    }
+    // To add a new post, just copy the block above and paste it here!
+];
