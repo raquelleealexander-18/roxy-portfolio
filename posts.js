3be038d16230b3a09b928ea84079b6b06878bc11
raquelleealexander-18 <raquelleealexander@gmail.com>
2026-02-13 09:03:48 -0500

    Add files via upload

---
+++ posts.js
@@ -0,0 +1,9 @@
+const blogPosts = [
+    {
+        title: "Spectacle and the Chameleon Nature of Ableism",
+        date: "February 12, 2026",
+        excerpt: "An Autistic queer woman’s critique of Love on the Spectrum, tokenization, and the gaze of the neurotypical observer.",
+        link: "blog/love-on-the-spectrum.html"
+    },
+    
+];
