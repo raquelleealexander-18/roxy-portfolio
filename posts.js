671fb139ef7718f8e146df50b4029e2207d5bd0a
raquelleealexander-18 <raquelleealexander@gmail.com>
2026-02-14 18:46:34 -0500

    website upgrade

--- posts.js
+++ posts.js
@@ -5,10 +5,5 @@ const blogPosts = [
         excerpt: "An Autistic queer woman’s critique of Love on the Spectrum, tokenization, and the gaze of the neurotypical observer.",
         link: "blogs/love-on-the-spectrum.html"
     },
-     {
-        title: "Pharmaceuticals: Technology, Machines, and Humans",
-        date: "December 2025",
-        excerpt: "My work calls for a shift in what we 'fix,' from curing individuals to transforming environments and social systems, reframing healthcare policy and pharmaceutical advertising through a pharmableist critique..",
-        link: "blogs/pharmaceuticals-technology-machines-and-humans.html"
-    },
+
 ];
