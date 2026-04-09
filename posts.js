893cab3277de4bb76a72b7f642914323a34320af
raquelleealexander-18 <raquelleealexander@gmail.com>
2026-04-09 17:49:18 -0400

    art

--- posts.js
+++ posts.js
@@ -1,5 +1,11 @@
 const blogPosts = [
     {
+        title: "A History of Hurst Hall's (In)Accessibility",
+        date: "April 9, 2026",
+        excerpt: "An investigation into the history of inaccessibility at American University's oldest building and the administration's continued complacency.",
+        link: "blogs/a-history-of-hurst-halls-inaccessibility.html"
+    },
+    {
         title: "Spectacle and the Chameleon Nature of Ableism",
         date: "February 12, 2026",
         excerpt: "An Autistic queer woman’s critique of Love on the Spectrum, tokenization, and the gaze of the neurotypical observer.",
